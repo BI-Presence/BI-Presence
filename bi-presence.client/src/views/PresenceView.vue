43f9fac41accc13408734a887214f8db0691cf09
Mochamad Syahrial Alzaidan <88835497+syahrialalzaidan@users.noreply.github.com>
2024-06-27 11:39:36 +0700

    feat: added login page

--- bi-presence.client/src/views/PresenceView.vue
+++ bi-presence.client/src/views/PresenceView.vue
@@ -164,7 +164,7 @@ const truncatedFileName = computed(() => {
       <!-- login -->
       <div class="pt-4 mb-3">
         <a
-          href="#/login"
+          href="/login"
           class="text-primary bg-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full border-2 border-primary sm:w-auto px-5 py-2.5 text-center dark:bg-white dark:hover:bg-slate-200 dark:hover:border-2 dark:hover:border-primary dark:hover:border-opacity-90 dark:hover:text-primary dark:focus:ring-blue-800 transition"
         >
           Login
